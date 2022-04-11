379c92eb631fae623840b54d23b5e4f1042d6d4e
Катерина <Me@example.com>
2022-04-11 23:47:47 +0300

    It works now

--- advice/advice_app/views.py
+++ advice/advice_app/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Post, Answer
 from .forms import PostForm
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-
 
 # Create your views here.
 def index(request):
@@ -30,18 +27,6 @@ def add_post(request):
     }
     return render(request, 'advice_app/add_post.html', context)
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('index')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+
 
 

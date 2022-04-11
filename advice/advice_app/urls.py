751bd8f76ed6ec0a02bb81877c6eb6454b98c99e
Катерина <Me@example.com>
2022-04-12 00:59:05 +0300

    registration works

--- advice/advice_app/urls.py
+++ advice/advice_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about_us', views.about_us, name='about'),
-    path('add_post', views.add_post, name='add_post')
+    path('add_post', views.add_post, name='add_post'),
+    url('register', views.register, name='register') 
 ]

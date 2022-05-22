55995a356d25d40873730837786194cc10aaa058
Катерина <Me@example.com>
2022-05-22 23:55:33 +0300

    Added dicstrings to the static methods

--- advice/advice_app/models.py
+++ advice/advice_app/models.py
@@ -63,22 +63,29 @@ class KeyWords(models.Model):
 
     @staticmethod
     def is_key_word(text):
+        """Сhecks if the word is a keyword."""
         isKeyword = len(text) > 1 and " " not in text or text.isdigit()
         return isKeyword
 
     @staticmethod
     def remove_punctuation(text):
+        """Removes punctuation for correct search function work."""
         text = text.replace(".", "").replace(",", "").replace("?", "").replace("!", "").replace(":", "").replace(";", "")
         return text
 
     @staticmethod
     def translate(text):
-        incorrect_input_dict = {"q":"й", "w":"ц", "e":"у", "r":"к", "t":"е", "y":"н", "u":"г", "i":"ш", "o":"щ", "p":"з", '[':"х",
-                                "]":"ї", "a":"ф", "s":"і", "d":"в", "f":"а", "g":"п", "h":"р", "j":"о", "k":"л", "l":"д", ";":"ж",
-                                "'":"є", "z":"я", "x":"ч", "c":"с", "v":"м", "b":"и", "n":"т", "m":"ь", ",":"б", ".":"ю", "`":"'",
-                                "Q":"Й", "W":"Ц", "E":"У", "R":"К", "T":"Е", "Y":"Н", "U":"Г", "I":"Ш", "O":"Щ", "P":"З", '{':"Х",
-                                "}":"Ї", "A":"Ф", "S":"І", "D":"В", "F":"А", "G":"П", "H":"Р", "J":"О", "K":"Л", "L":"Д", ":":"Ж",
-                                '"':"Є", "Z":"Я", "X":"Ч", "C":"С", "V":"М", "B":"И", "N":"Т", "M":"Ь", "<":"Б", ">":"Ю", "~":"'"}
+        """
+        Solves the problem of changing the layout by replacing the Latin alphabet with Cyrillic.
+        """
+        incorrect_input_dict = {
+            "q":"й", "w":"ц", "e":"у", "r":"к", "t":"е", "y":"н", "u":"г", "i":"ш", "o":"щ", "p":"з", '[':"х",
+            "]":"ї", "a":"ф", "s":"і", "d":"в", "f":"а", "g":"п", "h":"р", "j":"о", "k":"л", "l":"д", ";":"ж",
+            "'":"є", "z":"я", "x":"ч", "c":"с", "v":"м", "b":"и", "n":"т", "m":"ь", ",":"б", ".":"ю", "`":"'",
+            "Q":"Й", "W":"Ц", "E":"У", "R":"К", "T":"Е", "Y":"Н", "U":"Г", "I":"Ш", "O":"Щ", "P":"З", '{':"Х",
+            "}":"Ї", "A":"Ф", "S":"І", "D":"В", "F":"А", "G":"П", "H":"Р", "J":"О", "K":"Л", "L":"Д", ":":"Ж",
+            '"':"Є", "Z":"Я", "X":"Ч", "C":"С", "V":"М", "B":"И", "N":"Т", "M":"Ь", "<":"Б", ">":"Ю", "~":"'"
+        }
         new_text = ""
         for letter in text:
             if letter in incorrect_input_dict.keys():

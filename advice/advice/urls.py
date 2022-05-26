d8d39de91b476f549442f2b51f93d2a0f32f9428
Катерина <Me@example.com>
2022-05-26 18:00:39 +0300

    Added documentation

--- advice/advice/urls.py
+++ advice/advice/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('', include('advice_app.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
